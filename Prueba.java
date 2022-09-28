92f2b2615b6c54851e174d2f439a672f92bad131
Vasco <vasco.vjca@gmail.com>
2022-09-28 11:00:35 +0200

    Borrado apellidos y añadido grupo de musica favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 	
-		System.out.println("Cruz Alves");
 		System.out.println("20 años y 25 de diciembre de 2001");
 		System.out.println("Bacalao a brás");
+		System.out.println("Culture Club");
 
 	}
 
